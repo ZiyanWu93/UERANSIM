9d87d4e959c604d6f6fa2f26e9d37e252bcecf0e
ziyanwu <ziyanwu93@gmail.com>
2025-06-19 11:20:39 -0500

    feat: implement network function actors and event handlers for 5G core components

---
+++ src/core5g/udm_actor.c
@@ -0,0 +1,21 @@
+//
+// UDM Actor Implementation
+// Created on 2025-06-18
+//
+
+#include "event.h"
+
+// No existing UDM header file, would normally include it here
+
+// UDM functions for authentication vector generation
+EVENT_HANDLER(udm_decrypt_suci)
+{
+    // Recover SUPI from SUCI using home-network private key
+    // To be implemented
+}
+
+EVENT_HANDLER(udm_generate_5g_aka_vector)
+{
+    // Calculate RAND, AUTN, XRES* and Kseaf from long-term key K
+    // To be implemented
+}
